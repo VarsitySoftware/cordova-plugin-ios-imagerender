4ea5629fedba06ac2fef1320db6a0b6771176e06
Varsity Software <github@varsitysoftware.com>
2016-10-26 20:00:35 -0400

    Create imagerender.js

---
+++ www/imagerender.js
@@ -0,0 +1,26 @@
+var ImageRender = function ()
+{
+
+};
+
+ImageRender.prototype.run = function (success, fail, options)
+{
+    if (!options) {
+        options = {};
+    }
+
+    var params = {
+        tyoe: options.tyoe ? options.tyoe : 0
+        url: options.url ? options.url : null,
+        filter: options.filter ? options.filter : null,
+        cssPath: options.cssPath ? options.cssPath : null,
+        overlayBase64: options.overlayBase64 ? options.overlayBase64 : null,
+        loadDelay: options.loadDelay ? options.loadDelay : null,
+        quality: options.quality ? options.quality : 0
+    };
+
+    return cordova.exec(success, fail, "ImageRender", "run", [params]);
+
+};
+
+window.imageRender = new ImageRender();
